e9d230c9bd9827fe73e87ce77cf302da4c3024b9
Fabio Lucas Maciel <fabio.lucas1403@gmail.com>
2020-07-06 22:14:42 -0300

    Terminei a area de 2 cores
    - Adicionei o texto na area de 2 cores
    -Deixei a galeria com 4 imagens e diminui a margem entre eles
    -Adicionei JS nos botões

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var btnBanner = document.querySelector('input#btn-banner')
 var btnCurios = document.querySelector('input#btn-curios')
 var btnGal = document.querySelector('input#btn-gal')
+var btnAzulAula = document.querySelector('input.btn-azul')
+var btnLaranTorn = document.querySelector('input.btn-laranja')
 
 /*Botão do banner grande*/
 btnBanner.addEventListener('mouseenter',function(){
@@ -12,7 +14,7 @@ btnBanner.addEventListener('mouseout',function(){
 btnBanner.addEventListener('click',function(){
     btnBanner.style.backgroundColor = '#1b1b1b'
 })
-/*Botão do banner pequeno, aula teste*/
+/*Botão do banner pequeno, curiosidades*/
 btnCurios.addEventListener('mouseenter',function(){
     btnCurios.style.backgroundColor = '#863524'
 })
@@ -22,7 +24,27 @@ btnCurios.addEventListener('mouseout',function(){
 btnCurios.addEventListener('click',function(){
     btnCurios.style.backgroundColor = '#42180f'
 })
-/*Botão da gelria*/
+/*Botão laranja, aula teste*/
+btnLaranTorn.addEventListener('mouseenter',function(){
+    btnLaranTorn.style.backgroundColor = '#863524'
+})
+btnLaranTorn.addEventListener('mouseout',function(){
+    btnLaranTorn.style.backgroundColor = '#f06244'
+})
+btnLaranTorn.addEventListener('click',function(){
+    btnLaranTorn.style.backgroundColor = '#42180f'
+})
+/*Botão azul, torneio*/
+btnAzulAula.addEventListener('mouseenter',function(){
+    btnAzulAula.style.backgroundColor = '#0f1c31'
+})
+btnAzulAula.addEventListener('mouseout',function(){
+    btnAzulAula.style.backgroundColor = '#16294a'
+})
+btnAzulAula.addEventListener('click',function(){
+    btnAzulAula.style.backgroundColor = '#080f1a'
+})
+/*Botão da geleria*/
 btnGal.addEventListener('mouseenter',function(){
     btnGal.style.backgroundColor = '#863524'
 })
